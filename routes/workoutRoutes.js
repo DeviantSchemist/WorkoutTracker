b1549fc12a6715ecac48afa7e1e4fac77dc52b16
Deviant <michaelngo1337@yahoo.com>
2021-07-06 01:00:38 -0700

    added route to workout routes and fixed package.json

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,17 +1,10 @@
 const router = require('express').Router()
 const { Workout } = require('../models')
 
-// GETS ALL WORKOUTS ALONG WITH THEIR ASSOCIATED EXERCISES
+// GETS ALL WORKOUTS
 router.get('/workouts', (req, res) => {
   Workout.find()
     .then(workouts => res.json(workouts))
-    .catch(err => console.log(err))
-})
-
-// GETS A SPECIFIC WORKOUT ALONG WITH ITS ASSOCIATED EXERCISES
-router.get('/workouts/:id', (req, res) => {
-  Workout.findById(req.params.id)
-    .then(workout => res.json(workout))
     .catch(err => console.log(err))
 })
 
@@ -19,7 +12,17 @@ router.get('/workouts/:id', (req, res) => {
 router.get('/workouts/range', (req, res) => {
   Workout.find({ day: { $gte: new Date(new Date().setDate(new Date().getDate() - 7)), $lte: new Date(new Date().setDate(new Date().getDate())) }})
     .sort({ day: 1 })
-    .then(workouts => res.json(workouts))
+    .then(workouts => {
+      workouts.forEach(workout => {
+        workout.aggregate([
+          {
+            $addFields: { totalDuration: { $workout.exercises.duration } }
+          }
+        ])
+        .then(workouts => console.log(workouts))
+        .catch(err => console.log(err))
+      })
+    })
     .catch(err => console.log(err))
 })
 
@@ -30,16 +33,16 @@ router.post('/workouts', (req, res) => {
     .catch(err => console.log(err))
 })
 
-// UPDATES AN EXISTING WORKOUT WITH THE SPECIFIED ID
+// ADDS AN EXERCISE TO AN EXISTING WORKOUT
 router.put('/workouts/:id', (req, res) => {
-  Workout.findByIdAndUpdate(req.params.id, req.body)
-    .then(() => res.sendStatus(200))
+  Workout.findOne({ _id: req.params.id })
+    .then(workout => {
+      workout.exercises.push(req.body)
+      workout.save()
+        .then(() => res.sendStatus(200))
+        .catch(err => console.log(err))
+    })
     .catch(err => console.log(err))
 })
 
-// DELETES AN EXISTING WORKOUT
-router.delete('/workouts/:id', (req, res) => {
-  Workout.findByIdAndDelete(req.params.id)
-    .then(() => res.sendStatus(200))
-    .catch(err => console.log(err))
-})
+module.exports = router
